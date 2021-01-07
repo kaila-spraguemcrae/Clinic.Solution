b5844739dac790c8b49ce282876604f11c524777
Ellie Lambert <ebezjian@gmail.com>
2021-01-07 14:28:30 -0500

    add Create to Doctor Views and boilerplates
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>
    Co-authored-by: Shannon Grantski <grantski@pm.me>

--- Clinic/Controllers/DoctorsController.cs
+++ Clinic/Controllers/DoctorsController.cs
@@ -1,10 +1,36 @@
+using Clinic.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.EntityFrameworkCore;
-using Clinic.Models;
 
 namespace Clinic.Controllers
 {
-  
+  public class DoctorsController : Controller
+  {
+    private readonly ClinicContext _db;
+    public DoctorsController(ClinicContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Doctor> doctorList = _db.Doctors.ToList();
+      return View(doctorList);
+    }
+    
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Doctor doctor)
+    {
+      _db.Doctors.Add(doctor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
 }
