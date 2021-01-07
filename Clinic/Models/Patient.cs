d6edddc80bc9a212bb6bb2443a69546f2e50e27b
Ellie Lambert <ebezjian@gmail.com>
2021-01-07 12:59:06 -0500

    add function to Patient.cs in Models
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>
    Co-authored-by: Shannon Grantski <grantski@pm.me>

---
+++ Clinic/Models/Patient.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Clinic.Models
+{
+  public class Patient
+  {
+    public Patient()
+    {
+      this.Doctors = new HashSet<DoctorPatient>();
+    }
+    public int PatientId { get; set; }
+    public string PatientName { get; set; }
+    [DisplayName("Add AppointmentDate")]
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{yyyy-MM-dd}")]
+    public DateTime AppointmentDate { get; set; }
+    public ICollection<DoctorPatient> Doctor {get;}
+  }
+}
